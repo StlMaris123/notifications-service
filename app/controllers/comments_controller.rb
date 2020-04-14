54af4262e136b7f88c534669625112599a49472f
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-14 02:33:24 +0200

    implement comment section on the discusiion forums

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+    before_action :find_commentable
+
+  def new
+    @forum = Forum.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def create
+    @commentable.comments.build(comment_params)
+    if @commentable.save
+    else
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Forum.find_by_id(params[:forum_id]) if params[:forum_id]
+  end
+end
