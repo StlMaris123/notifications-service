020c345ae44fe6afea1b4ff1bd8f700df7f24ab0
StlMaris123 <stellamarisnjage@gmail.com>
2020-04-21 18:05:59 +0200

    access current_user in the service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :find_current_user
+
+  private
+
+  def find_current_user
+    @current_user = User.find(session['warden.user.user.key'].first)
+  end
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.config.session_store :redis_session_store, {
+  key: '_be_free_session',
+  serializer: :json,
+  domain: :all,
+  redis: {
+    expire_after: 1.week,
+    key_prefix: 'be_free:session',
+    url: ENV['REDIS_SESSIONS_URL']
+  }
+}
