c268292bd97a8d7656557bfd5426c45cd8c3b08f
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-06 21:57:00 +0200

    match up prd variables to what is on prd

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@
 Rails.application.config.session_store :redis_session_store, {
   key: '_be_free_session',
   serializer: :json,
-  domain: (ENV['HOST']),
+  
   redis: {
     expire_after: 1.week,
     key_prefix: 'be_free:session',
