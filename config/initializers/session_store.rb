2b9c42a17b0f915313e7eaed3338bcac1c12c06f
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-06 21:21:58 +0200

    add host to domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,7 @@
 Rails.application.config.session_store :redis_session_store, {
   key: '_be_free_session',
   serializer: :json,
+  domain: URI(ENV['HOST']).host,
   redis: {
     expire_after: 1.week,
     key_prefix: 'be_free:session',
