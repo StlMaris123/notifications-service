2dc2f8022c5220cdd93afbd4bb5449cdbec5ea9c
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-06 19:54:21 +0200

    invoke current user

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,6 @@
 Rails.application.config.session_store :redis_session_store, {
   key: '_be_free_session',
   serializer: :json,
-  domain: :all,
   redis: {
     expire_after: 1.week,
     key_prefix: 'be_free:session',
