9d92531a79267ae4e39cd66a6e9c20fc49d6c42e
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-26 16:55:23 +0200

    authenticity token being attached to url because of form syntax from bootsrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery prepend: true
   skip_before_action :verify_authenticity_token, if: :json_request?
   
   before_action :find_current_user
